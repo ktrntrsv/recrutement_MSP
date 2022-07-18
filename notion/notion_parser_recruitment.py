20f1a21939c812c124ec60cad58ac4f2a768dde1
ktrntrsv <ktrntrsv@gmail.com>
2022-07-18 17:42:29 +0300

    Fixed GS retake bug

--- notion/notion_parser_recruitment.py
+++ notion/notion_parser_recruitment.py
@@ -47,12 +47,6 @@ class NotionParserRecruitment(NotionParser):
         }
         self.body["filter"]["and"].append(prop_dict)
 
-    @staticmethod
-    def get_max_date(date: list) -> str:
-        for ind, day in enumerate(date):
-            date[ind] = datetime.fromisoformat(day)
-        return str(max(date))[:10]
-
     def get_filtered_fields_meaning(self) -> any((list, None)):
         """
         Get from self.db_info fields (which specified in config.field_names)
@@ -88,6 +82,7 @@ class NotionParserRecruitment(NotionParser):
         """
 
         ind_to_pop = self.get_indexes_of_popped_preps(info)
+        self.fix_coming_date(info)
 
         for ind in ind_to_pop[::-1]:
             info.pop(ind)
@@ -106,3 +101,18 @@ class NotionParserRecruitment(NotionParser):
             if (date - self.end_day).days > 0 or (self.start_day - date).days > 0:
                 ind_to_pop.append(i)
         return ind_to_pop
+
+    @staticmethod
+    def get_max_date(date: list) -> any((None, str)):
+        if not date:
+            return None
+        for ind, day in enumerate(date):
+            date[ind] = datetime.fromisoformat(day)
+        return str(max(date))[:10]
+
+    @staticmethod
+    def fix_coming_date(prep_list):
+        for i, prep in enumerate(prep_list):
+            if prep["ГС: дата прихода ₓ"] and \
+                    prep["ГС: дата прихода ₓ"] != prep["ГС: дата приглашения ₓ"]:
+                prep["ГС: дата прихода ₓ"] = []
