e06e5966985774f9bb6bc20a6ad8f8a396feac4f
ktrntrsv <ktrntrsv@gmail.com>
2022-10-04 19:10:05 +0300

    Oops, circular import

--- list_name.py
+++ list_name.py
@@ -1,5 +1,3 @@
-from logger_file import logger
-
 import sys
 from datetime import datetime, timedelta
 
@@ -41,10 +39,8 @@ def get_list_name(prev_sem: bool = False):
 def is_spb_flag() -> bool:
     arguments = sys.argv
     if not len(arguments) >= 2:
-        logger.info("Spb_flag = False")
         return False
     if arguments[1] == "--spb":
-        logger.info("Spb_flag = True")
         return True
 
 
